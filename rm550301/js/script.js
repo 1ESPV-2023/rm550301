63d19012c34031b1f8d179d5507333af451746a3
predowss <rm550301@fiap.com.br>
2023-04-27 20:10:21 -0300

    troca de banners

--- rm550301/js/script.js
+++ rm550301/js/script.js
@@ -4,27 +4,50 @@
 
 // console.log(soma(15, 23));
 
-let tmp = "";
+// let tmp = "";
 
-function mudaCor() {
+// function mudaCor() {
+
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
+
+//     //Math
+//     r = Math.ceil(Math.random() * 255);
+//     g = Math.ceil(Math.random() * 255);
+//     b = Math.ceil(Math.random() * 255);
+
+//     const elementos = [...document.getElementsByClassName("cabecalho")];
+
+//     elementos.forEach((el)=>{
+//         el.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     })
+
+//     tmp = setTimeout(mudaCor, 50);
+
+// }
+
+// mudaCor()
+
+function changeBanner() {
     
-    let r = 0;
-    let g = 0;
-    let b = 0;
+    let nr1 = Math.ceil(Math.random()*3);
+    let nr2 = Math.ceil(Math.random()*3);
+    //Randomiza um numero entre 1 e 3 para escolher as imagens
 
-    //Math
-    r = Math.ceil(Math.random() * 255);
-    g = Math.ceil(Math.random() * 255);
-    b = Math.ceil(Math.random() * 255);
+    let caminhoImg1 = `./img/banner-lateral-${nr1}.png`;
+    let caminhoImg2 = `./img/banner-lateral-${nr2}.png`;
+    //Troca a imagem dos banners
+   
+    const b1 = document.querySelector(".banner-1 img");
+    const b2 = document.querySelector(".banner-2 img");
+    b1.src = caminhoImg1;
+    b2.src = caminhoImg2;
 
-    const elementos = [...document.getElementsByClassName("cabecalho")];
-    
-    elementos.forEach((el)=>{
-        el.style.backgroundColor = `rgb(${r},${g},${b})`;
-    })
-
-    tmp = setTimeout(mudaCor, 50);
-
+    setTimeout(mudaBanner1 , 1000);
+    //Troca a imagem a cada 1 segundo
 }
-    
-mudaCor()
+
+changeBanner();
+
+//Math.ceil arredonda pra cima
